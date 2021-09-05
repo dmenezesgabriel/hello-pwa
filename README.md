d5e056d9b1eda222454203a789f4b2ff51f1fb0c
Gabriel <dmenezes.gabriel@gmail.com>
2021-09-05 07:54:53 -0300

    Add First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello WPA
+
+## Requirements
+
+- The app should display some content even if Javascript is disabled, what prevents users from seeing a blank page if their internet connection is bad or they are using an old browser.
+
+- Should be responsible and mobile friendly
---
+++ serviceWorker.js
@@ -0,0 +1,28 @@
+// service workers only have permission to access files
+// in the same directory and sub - directories.
+
+var cacheName = "hello-pwa";
+var filesToCache = [
+  "/",
+  "index.html",
+  "/assets/css/styles.css",
+  "/assets/js/main.js",
+];
+
+// service workers only have permission to access files in the same directory and sub-directories.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// service workers only have permission to access files in the same directory and sub-directories.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
